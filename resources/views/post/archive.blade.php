0f4704128fd3bbf948e91a5ec2f2b3ef854829c9
gobanoff <a.kliosovas@yahoo.com>
2025-03-14 18:24:29 +0200

    a

--- resources/views/post/archive.blade.php
+++ resources/views/post/archive.blade.php
@@ -84,6 +84,7 @@
                                                     @csrf
                                                     <button type="submit"class="btn btn-outline-danger">DELETE</button>
                                                 </form>
+                                                <button type="submit"class="btn btn-outline-success">Your Post</button>
                                             @endif
 
                                         </div>
