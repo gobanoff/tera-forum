578b9c694a40d0091cc12e032d8c8bb563c9fe5c
gobanoff <a.kliosovas@yahoo.com>
2025-03-27 13:16:12 +0200

    Docker deployment to Vercel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container"style="display: grid; grid-template-columns: 1fr 400px;">
+        <div>
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-10">
                 <div class="card">
                     <div class="card-header"> <a href="{{ route('author.index') }}"> Authors</a></div>
                     <div class="card-header"> <a href="{{ route('post.index') }}"> FK"Tera" Forum</a></div>
@@ -23,9 +24,20 @@
 
                         <b style="color:rgb(33, 201, 25);">
                             {{ __('You are logged in!') }}</b>
+
+
+                           
+
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <div>
+       <!-- <iframe src="http://localhost:8501" width="110%" height="500px" frameborder="0"></iframe>-->
+    </div>
+</div>
+   
 @endsection
